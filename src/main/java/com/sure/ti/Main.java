03c735f89d7d4b7c730004fdbe5f260b1232475b
Lucas Coelho <58010418+LucasVinicius314@users.noreply.github.com>
2021-09-09 23:18:04 -0300

    initial

---
+++ src/main/java/com/sure/ti/Main.java
@@ -0,0 +1,41 @@
+package com.sure.ti;
+
+public class Main {
+    public static void main(String[] args) {
+        DAO dao = new DAO();
+
+        dao.conectar();
+
+        dao.init();
+
+        Usuario usuario = new Usuario(11, "pablo", "pablo", 'M');
+        if (dao.inserirUsuario(usuario) == true) {
+            System.out.println("Inserção com sucesso -> " + usuario.toString());
+        }
+
+        System.out.println("==== Mostrar usuários do sexo masculino === ");
+        Usuario[] usuarios = dao.getUsuariosMasculinos();
+        for (int i = 0; i < usuarios.length; i++) {
+            System.out.println(usuarios[i].toString());
+        }
+
+        usuario.setSenha("nova senha");
+        dao.atualizarUsuario(usuario);
+
+        System.out.println("==== Mostrar usuários === ");
+        usuarios = dao.getUsuarios();
+        for (int i = 0; i < usuarios.length; i++) {
+            System.out.println(usuarios[i].toString());
+        }
+
+        dao.excluirUsuario(usuario.getCodigo());
+
+        usuarios = dao.getUsuarios();
+        System.out.println("==== Mostrar usuários === ");
+        for (int i = 0; i < usuarios.length; i++) {
+            System.out.println(usuarios[i].toString());
+        }
+
+        dao.close();
+    }
+}
